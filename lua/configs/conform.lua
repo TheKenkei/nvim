2df2478c82937f95bb56143dd0e773aeddf0242e
TheKenkei <nowikow.witaliy@gmail.com>
2024-03-26 00:14:05 +0300

    feat: add formatter configs

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,25 @@
 local options = {
   formatters_by_ft = {
+    typescript = { "prettierd" },
+    javascript = { "prettierd" },
+
+    html = { "prettierd" },
+    css = { "prettierd" },
+    scss = { "prettierd" },
+
+    json = { "prettierd" },
+
+    php = { "ecs" },
+    yaml = { "prettierd" },
+    xml = { "xmlformat" },
+
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
