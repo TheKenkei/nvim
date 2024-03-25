fa5d53ff6d1d3311151d8aba8729c7cb76504734
TheKenkei <nowikow.witaliy@gmail.com>
2024-03-26 00:13:45 +0300

    feat: add `lspconfig` config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,12 +1,23 @@
--- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
-
+local util = require "lspconfig.util"
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
 
--- lsps with default config
+--ANGULAR--
+local install_path = vim.fn.stdpath "data" .. "/mason/packages/angular-language-server/node_modules"
+local ang = install_path .. "/@angular/language-server/node_modules"
+local cmd = { "ngserver", "--stdio", "--tsProbeLocations", install_path, "--ngProbeLocations", ang }
+--ANGULAR--
+
+local servers = {
+  "html",
+  "cssls",
+  "clangd",
+  "intelephense",
+  "phpactor",
+}
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
@@ -20,4 +31,19 @@ lspconfig.tsserver.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+}
+
+-- tailwindcss
+lspconfig.tailwindcss.setup {
+  require "configs.tailwindcss",
+}
+
+--angularls
+lspconfig.angularls.setup {
+  on_attach = on_attach,
+  cmd = cmd,
+  new_root_dir = util.root_pattern("angular.json", "project.json"),
+  on_new_config = function(new_config, new_root_dir)
+    new_config.cmd = cmd
+  end,
 }
