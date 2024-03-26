0f3fe47dfb34b223fa87e0161ac6e6f9635bc6ac
TheKenkei <nowikow.witaliy@gmail.com>
2024-03-26 22:44:02 +0300

    fix: edit `Lazygit` shortcut

--- lua/mappings/git.lua
+++ lua/mappings/git.lua
@@ -2,5 +2,5 @@ local map = vim.keymap.set
 map("n", "<leader>gg", function()
   local worktree = require("utils.git").file_worktree()
   local flags = worktree and (" --work-tree=%s --git-dir=%s"):format(worktree.toplevel, worktree.gitdir) or ""
-  require("nvchad.term").toggle { pos = "float", id = "floatTerm", cmd = "lazygit " .. flags .. ";exit" }
+  require("nvchad.term").new { pos = "float", id = "floatTerm", cmd = "lazygit " .. flags .. ";exit" }
 end, { desc = "Lazygit" })
