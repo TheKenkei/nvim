cc9e6102bcbbc0e549d1c4bc3107b6305d885e5f
TheKenkei <nowikow.witaliy@gmail.com>
2024-03-26 20:42:46 +0300

    fix: current config for `php-cs-fixer`

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,3 +1,23 @@
+local util = require "conform.util"
+require("conform").setup {
+  formatters = {
+    phpcsfixer = {
+      meta = {
+
+        url = "https://github.com/PHP-CS-Fixer/PHP-CS-Fixer",
+        description = "The PHP Coding Standards Fixer.",
+      },
+      command = util.find_executable({
+        "tools/php-cs-fixer/vendor/bin/php-cs-fixer",
+        "vendor/bin/php-cs-fixer",
+      }, "php-cs-fixer"),
+      args = { "fix", "$FILENAME" },
+      stdin = false,
+      cwd = util.root_file { "composer.json" },
+      -- Change where to find the command
+    },
+  },
+}
 local options = {
   formatters_by_ft = {
     typescript = { "prettierd" },
@@ -9,7 +29,7 @@ local options = {
 
     json = { "prettierd" },
 
-    php = { "ecs" },
+    php = { "phpcsfixer" },
     yaml = { "prettierd" },
     xml = { "xmlformat" },
 
--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -1,6 +1,7 @@
 local options = {
   filters = {
     dotfiles = false,
+    custom = { ".conform*" },
     exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
   },
   disable_netrw = true,
