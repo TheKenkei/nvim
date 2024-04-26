bbb2023f65973e22539ed461e9380bff472aed93
TheKenkei <nowikow.witaliy@gmail.com>
2024-04-26 21:05:24 +0300

    feat:new mappings

--- lua/mappings/nvim.lua
+++ lua/mappings/nvim.lua
@@ -5,6 +5,7 @@ del("n", "<leader>n")
 del("n", "<leader>rn")
 map("n", "<A-q>", "<cmd>confirm q<cr>", { desc = "Quit" })
 map("n", "\\", "<cmd>:vsplit <CR>", { desc = "Vertical split" })
+map("n", "<leader>ut", "<cmd>Telescope themes<CR>", { desc = "Telescope Nvchad themes" })
 
 vim.keymap.set("i", "<C-g>", function()
   return vim.fn["codeium#Accept"]()
