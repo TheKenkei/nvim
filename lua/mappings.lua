ac026babf3391e46d33491309853dbd6b7bb7c79
TheKenkei <kenkei.message@gmail.com>
2024-09-01 09:26:05 +0300

    fix: mapping for close all bufs

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,6 +9,9 @@ map("n", "<c-k>", "<cmd>:TmuxNavigateUp<cr>", { desc = "Tmux Up" })
 map("n", "<c-j>", "<cmd>:TmuxNavigateDown<cr>", { desc = "Tmux Down" })
 --  vvvvvv TmuxNavigate vvvvvv
 
+--  ^^^^^^ Eslint ^^^^^^
+map("n", "<leader>le", "<cmd>EslintFixAll<cr>", { desc = "Eslint fix all problem" })
+--  vvvvvv Eslint vvvvvv
 --  ^^^^^^ Telescope ^^^^^^
 local builtin = require "telescope.builtin"
 map("n", "<leader><leader>", builtin.find_files, { desc = "Find files" })
@@ -23,12 +26,11 @@ map("n", "<A-g>", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 map("n", "\\", "<cmd>:vsplit <CR>", { desc = "Vertical split" })
-map("n", "<leader>bC", function()
-  require("nvchad.tabufline").closeAllBuf()
+map("n", "<leader>q", function()
+  require("nvchad.tabufline").closeAllBufs()
 end, { desc = "Close Other Tabs" })
 map("i", "jj", "<ESC>")
 map("n", "<A-q>", "<cmd>confirm q<cr>", { desc = "Quit" })
-map("n", "<leader>qq", "<cmd>confirm qa<cr>", { desc = "Quit" })
 map("n", "<C-A-q>", "<cmd>confirm qa!<cr>", { desc = "Quit" })
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("i", "<CA-h>", "<C-Left>", { desc = "Left" })
