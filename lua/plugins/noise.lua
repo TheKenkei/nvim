52a6bb6ef1466b3a0240dffc3ca349ee2309d9af
TheKenkei <kenkei.message@gmail.com>
2024-10-14 23:27:05 +0300

    feat(plugins): update `noise` plugin

--- lua/plugins/noise.lua
+++ lua/plugins/noise.lua
@@ -2,6 +2,7 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
+
     cmdline = {
       enabled = true, -- enables the Noice cmdline UI
       view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
@@ -26,7 +27,7 @@ return {
     messages = {
       -- NOTE: If you enable messages, then the cmdline is enabled automatically.
       -- This is a current Neovim limitation.
-      enabled = false, -- enables the Noice messages UI
+      enabled = true, -- enables the Noice messages UI
       view = "notify", -- default view for messages
       view_error = "notify", -- view for errors
       view_warn = "notify", -- view for warnings
@@ -34,7 +35,7 @@ return {
       view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
     },
     popupmenu = {
-      enabled = false, -- enables the Noice popupmenu UI
+      enabled = true, -- enables the Noice popupmenu UI
       ---@type 'nui'|'cmp'
       backend = "nui", -- backend to use to show regular cmdline completions
       ---@type NoicePopupmenuItemKind|false
@@ -101,7 +102,7 @@ return {
       -- event is always "notify" and kind can be any log level as a string
       -- The default routes will forward notifications to nvim-notify
       -- Benefit of using Noice for this is the routing and consistent history view
-      enabled = false,
+      enabled = true,
       view = "notify",
     },
     lsp = {
@@ -183,17 +184,57 @@ return {
     presets = {
       -- you can enable a preset by setting it to true, or a table that will override the preset config
       -- you can also add custom presets that you can enable/disable with enabled=true
-      bottom_search = false, -- use a classic bottom cmdline for search
-      command_palette = false, -- position the cmdline and popupmenu together
-      long_message_to_split = false, -- long messages will be sent to a split
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
     },
     throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
     ---@type NoiceConfigViews
-    views = {}, ---@see section on views
+    views = {
+
+      cmdline_popup = {
+        position = {
+          row = 5,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+    }, ---@see section on views
     ---@type NoiceRouteConfig[]
-    routes = {}, --- @see section on routes
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "written",
+        },
+        opts = { skip = true },
+      },
+    },
+
     ---@type table<string, NoiceFilter>
     status = {}, --- @see section on statusline components
     ---@type NoiceFormatOptions
---
+++ lua/plugins/notify.lua
@@ -0,0 +1,6 @@
+return {
+  "rcarriga/nvim-notify",
+  opts = {
+    background_colour = "#1E1E1E",
+  },
+}
