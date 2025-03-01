d3b6e5ee81af4155da0b9b59b5a210345c8986b2
TheKenkei <kenkei.message@gmail.com>
2025-03-01 21:57:55 +0300

    chore: update charc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -14,12 +14,73 @@ M.base46 = {
         ["@comment"] = { italic = true },
     },
 }
+M.term = {
+    sizes = {
+        sp = 1,
+        vsp = 1,
+    },
+}
+M.ui = {
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+    statusline = {
+        theme = "minimal", -- default/vscode/vscode_colored/minimal
+        -- default/round/block/arrow separators work only for default statusline theme
+        -- round and block will work for minimal theme only
+        separator_style = "round",
+        order = nil,
+        -- modules = nil,
+    },
+
+    -- lazyload it when there are 1+ buffers
+    tabufline = {
+        enabled = true,
+        lazyload = false,
+        order = { "treeOffset", "buffers", "tabs", "btns" },
+        modules = nil,
+    },
+}
+
+M.nvdash = {
+    load_on_startup = true,
+
+    header = {
+        "                            ",
+        " ██   ██ ███████ ███    ██ ██   ██ ███████ ██ ",
+        " ██  ██  ██      ████   ██ ██  ██  ██      ██ ",
+        " █████   █████   ██ ██  ██ █████   █████   ██ ",
+        " ██  ██  ██      ██  ██ ██ ██  ██  ██      ██ ",
+        " ██   ██ ███████ ██   ████ ██   ██ ███████ ██ ",
+        "                            ",
+        "     Powered By  eovim    ",
+        "                            ",
+    },
+    buttons = {
+        { txt = "  Git diff files", keys = "g", cmd = "Telescope git_status " },
+        { txt = "  Find File", keys = "f", cmd = "Telescope find_files" },
+        { txt = "󰈭  Find Word", keys = "w", cmd = "Telescope live_grep" },
+
+        { txt = "󰏔  Mason", keys = "m", cmd = "Mason" },
+        { txt = "󰏗  Lazy", keys = "l", cmd = "Lazy" },
+
+        { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+        { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+
+        { txt = "Quit", keys = "q", cmd = "q" },
+
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+        {
+
+            txt = function()
+                local stats = require("lazy").stats()
+                local ms = math.floor(stats.startuptime) .. " ms"
+                return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+            end,
+            hl = "NvDashFooter",
+            no_gap = true,
+        },
+
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    },
+}
 
 return M
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,18 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
+local del = vim.keymap.del
 local tabs = require "nvchad.tabufline"
 
+local function cmd(opt)
+    return "<cmd>" .. opt .. "<cr>"
+end
+del("n", "<leader>h")
+del("n", "<leader>v")
+
 map("i", "jk", "<ESC>")
+map("n", "<leader>qq", cmd "qa", { desc = "Quit" })
+map("n", "<leader>gh", cmd "Nvdash", { desc = "Open star page" })
 
 map("n", "<leader>qa", function()
     tabs.closeAllBufs(false)
@@ -18,8 +27,8 @@ end, { desc = "Close Other Tabs" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Center cursor before scroll" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Center cursor before scroll" })
 
-map("n", "sv", "<cmd>:vsplit <CR>", { desc = "Vertical split" })
-map("n", "sh", "<cmd>:split <CR>", { desc = "Vertical split" })
+map("n", "sv", cmd ":vsplit", { desc = "Vertical split" })
+map("n", "sh", cmd ":split", { desc = "Vertical split" })
 
 map("n", "<C-a>", "ggVG", { desc = "Select all" })
 
@@ -38,32 +47,33 @@ map("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = "m
 map("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true, desc = "move line up" })
 map("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true, desc = "move line up" })
 map("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = "move line up" })
-map("v", "<leader>tr", "<cmd>:Translate ru<cr>", { desc = "Translate to rus" })
-map("n", "<leader><leader>", "<cmd>:Telescope lsp_document_symbols<cr>", { desc = "find lsp symbols" })
-map("n", "<A-/>", "<cmd>:Telescope current_buffer_fuzzy_find<cr>", { desc = "current_buffer_fuzzy_find" })
+map("v", "<leader>tr", cmd ":Translate ru", { desc = "Translate to rus" })
+map("n", "<leader><leader>", cmd ":Telescope lsp_document_symbols", { desc = "find lsp symbols" })
+map("n", "<A-/>", cmd ":Telescope current_buffer_fuzzy_find", { desc = "current_buffer_fuzzy_find" })
 
-map("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
-map("n", "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "Git blame toggle" })
+map("n", "<leader>gg", cmd "LazyGit ", { desc = "LazyGit" })
+map("n", "<leader>gb", cmd "Gitsigns toggle_current_line_blame", { desc = "Git blame toggle" })
 
-map({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>", { desc = "Save" })
-map({ "n", "i", "v" }, "<CA-s>", "<cmd>wa<cr>", { desc = "Save all files" })
+map({ "n", "i", "v" }, "<C-s>", cmd "w", { desc = "Save" })
+map({ "n", "i", "v" }, "<CA-s>", cmd "wa", { desc = "Save all files" })
 
-map("n", "<A-q>", "<cmd>confirm q<cr>", { desc = "Quit" })
-map("n", "<C-A-q>", "<esc><esc><cmd>confirm qa!<cr>", { desc = "Quit" })
+map("n", "<A-q>", cmd "confirm q", { desc = "Quit" })
+map("n", "<C-A-q>", cmd "confirm qa!", { desc = "Quit" })
+
+map("n", "c", '"_c', { noremap = true })
+map("n", "C", '"_C', { noremap = true })
+
+map("n", "<leader>le", cmd ":EslintFixAll", { desc = "Eslint" })
+map("n", "<leader>lr", cmd ":LspUI rename", { desc = "Rename" })
+map("n", "]]", cmd ":LspUI diagnostic next", { desc = "Next diagnostic" })
+map("n", "[[", cmd ":LspUI diagnostic prev", { desc = "Prev diagnostic" })
+
+--  ^^^^^^ TmuxNavigate ^^^^^^
+map("n", "<c-l>", cmd ":TmuxNavigateRight", { desc = "Tmux Right" })
+map("n", "<c-h>", cmd ":TmuxNavigateLeft", { desc = "Tmux Left" })
+map("n", "<c-k>", cmd ":TmuxNavigateUp", { desc = "Tmux Up" })
+map("n", "<c-j>", cmd ":TmuxNavigateDown", { desc = "Tmux Down" })
 
 map("n", "<C-g>", function()
     return vim.fn["codeium#Accept"]()
 end, { desc = "Codeium accesst" })
-map("n", "c", '"_c', { noremap = true })
-map("n", "C", '"_C', { noremap = true })
-
-map("n", "<leader>le", "<cmd>:EslintFixAll<cr>", { desc = "Eslint" })
-map("n", "<leader>lr", "<cmd>:LspUI rename<cr>", { desc = "Rename" })
-map("n", "]]", "<cmd>:LspUI diagnostic next<cr>", { desc = "Next diagnostic" })
-map("n", "[[", "<cmd>:LspUI diagnostic prev<cr>", { desc = "Prev diagnostic" })
-
---  ^^^^^^ TmuxNavigate ^^^^^^
-map("n", "<c-l>", "<cmd>:TmuxNavigateRight<cr>", { desc = "Tmux Right" })
-map("n", "<c-h>", "<cmd>:TmuxNavigateLeft<cr>", { desc = "Tmux Left" })
-map("n", "<c-k>", "<cmd>:TmuxNavigateUp<cr>", { desc = "Tmux Up" })
-map("n", "<c-j>", "<cmd>:TmuxNavigateDown<cr>", { desc = "Tmux Down" })
