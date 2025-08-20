91f7903a182ac87b9f1150f7233b974758db1b68
TheKenkei <kenkei.message@gmail.com>
2025-08-21 00:23:05 +0300

    chore: update lsp configs

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -14,6 +14,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
--- vim.api.nvim_create_autocmd({ "FocusLost", "FocusGained" }, {
---     command = "checktime",
--- })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.ts, *html",
+
+    callback = function()
+        vim.cmd "LspEslintFixAll"
+    end,
+})
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,39 +2,21 @@ local install_path = vim.fn.stdpath "data" .. "/mason/packages/angular-language-
 local ang = install_path .. "/@angular/language-server/node_modules"
 local angular_cmd = { "ngserver", "--stdio", "--tsProbeLocations", install_path, "--ngProbeLocations", ang }
 
-local base_on_attach = vim.lsp.config.eslint.on_attach
 local servers = {
     "html",
-    -- "vtsls",
     "lemminx",
     "bashls",
     "css_variables",
     "jsonls",
     "emmet_language_server",
-    "lemminx",
     "tailwindcss",
     "cssls",
     "eslint",
     "angularls",
-    "tsgo",
     "ts_ls",
 }
 
 require("nvchad.configs.lspconfig").defaults()
-
-vim.lsp.config("eslint", {
-    on_attach = function(client, bufnr)
-        if not base_on_attach then
-            return
-        end
-
-        base_on_attach(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "LspEslintFixAll",
-        })
-    end,
-})
 
 vim.lsp.config("angularls", {
     cmd = angular_cmd,
